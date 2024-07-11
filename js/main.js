3594abb8a0b5d947fba988cb7d7b78ff5997535b
ulises <ulisestomas028@gmail.com>
2024-07-11 14:22:01 -0300

    subiendo al repo

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let nombre = prompt ("ingrese su nombre");
+let apellido = prompt ("ingrese su apellido");
+
+alert ("bienvenido," + nombre + " " + apellido);
+
+// let numeroA = parseInt(prompt ("ingresa el primer numero"));
+// let numeroB = parseInt(prompt ("ingresa el segundo numero"));
+
+// let suma = numeroA + numeroB;
+
+// alert ("la suma es: " + suma);
+
+let numeroA = parseInt(prompt ("ingresa el primer numero"));
+let numeroB = parseInt(prompt ("ingresa el segundo numero"));
+
+let resta = numeroA - numeroB;
+
+alert ("la suma es: " + resta);
